39024a6589d90d76051722087606257945d6f634
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-29 22:01:19 +0200

    Move header content to header file and import it from there instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header"
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="app h-screen">
+      <Header />
+    </div>
   )
 }
 
