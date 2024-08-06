78dc33afa656553889db4e46cfc3be1b247d739d
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-06 20:57:00 +0200

    Add CarouselButton component to carousel in newArrivalsContainer, also load product data from sneaks-api to each button

--- src/components/Main/NewArrivalsContainer.jsx
+++ src/components/Main/NewArrivalsContainer.jsx
@@ -1,4 +1,4 @@
-import MainButton from "./ShowCaseButton";
+import CarouselButton from "./CarouselButton";
 import { getMostPopularSneakers } from "../../api";
 import { useState, useEffect } from "react";
 import Slider from "react-slick";
@@ -6,49 +6,53 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function NewArrivalsContainer() {
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-    }
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
 
-    const [sneakers, setSneakers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [sneakers, setSneakers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    getMostPopularSneakers()
+      .then((data) => {
+        console.log(data);
+        setSneakers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
-    useEffect(() => {
-        getMostPopularSneakers() 
-          .then((data) => {
-            setSneakers(data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            setError(error);
-            setLoading(false);
-          });
-      }, []);
-    
-
-
-    return (
-        <div className="flex flex-col gap-4 justify-center ">
-            <h2 className="text-black ml-48 text-4xl font-bold">New Arrivals</h2>
-           <div>
-                <div className="w-full px-56">
-                    <Slider {...sliderSettings}>
-                    {sneakers.map((sneaker) => (
-                        <div key={sneaker._id} className="bg-black p-8">
-                            < MainButton imgSrc={sneaker.thumbnail} alt={sneaker.shoeName} height='h-52'/>
-                        </div>
-                    ))}
-                    </Slider>
-                </div>
-           </div>
+  return (
+    <div className="flex flex-col gap-4 justify-center ">
+      <h2 className="text-black ml-48 text-4xl font-bold">New Arrivals</h2>
+      <div>
+        <div className="w-full px-56">
+          <Slider {...sliderSettings}>
+            {sneakers.map((sneaker) => (
+              <div key={sneaker._id} className="p-8">
+                <CarouselButton
+                  imgSrc={sneaker.thumbnail}
+                  alt={sneaker.shoeName}
+                  brand={sneaker.brand}
+                  name={sneaker.shoeName}
+                  price={sneaker.lowestResellPrice.stockX}
+                />
+              </div>
+            ))}
+          </Slider>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default NewArrivalsContainer;
