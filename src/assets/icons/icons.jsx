e605758bc0a7ed9d711d50b1864b455c990c6898
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-06 21:16:30 +0200

    Add arrow icons for carousel buttons

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -24,7 +24,16 @@ const icons = {
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
     ),
-
+    chevronLeft: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="black" className="w-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+    ),
+    chevronRight: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="black" className="w-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+    ),
 }
 
 export default icons;
