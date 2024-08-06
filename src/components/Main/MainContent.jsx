a6f0f2bf0eb6936acd7807395b7d612ad75a6c76
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-06 20:32:32 +0200

    Add 'NewArrivlasContainer' component to MainContent component

--- src/components/Main/MainContent.jsx
+++ src/components/Main/MainContent.jsx
@@ -1,9 +1,11 @@
+import NewArrivalsContainer from "./NewArrivalsContainer";
 import ShowcaseContainer from "./ShowcaseContainer";
 
 function MainContent() {
     return (
-        <div>
+        <div className="flex flex-col gap-12">
             < ShowcaseContainer />
+            < NewArrivalsContainer />
         </div>
     )
 }
