5933e34510558ec7f52772c2b41c6ec236b1ea95
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-13 23:22:40 +0200

    Create footer folder and jsx file that will hold footer content

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,60 @@
+function Info() {
+  return (
+    <div>
+      <div>
+        <h1>INFO</h1>
+      </div>
+      <div>
+        <p>Contact Us</p>
+        <p>Releases</p>
+        <p>Stores</p>
+        <p>Brands</p>
+        <p>Blogs</p>
+      </div>
+    </div>
+  );
+}
+
+function Policies() {
+  return (
+    <div>
+      <div>
+        <h1>POLICIES</h1>
+      </div>
+      <div>
+        <p>Help Center</p>
+        <p>Privacy Policy</p>
+        <p>Returns & Exchange</p>
+        <p>Terms & Conditions</p>
+        <p>Order & Shipping</p>
+        <p>Terms of Service</p>
+      </div>
+    </div>
+  );
+}
+
+function Socials() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+function Newsletter() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Footer;
