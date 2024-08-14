216a2cc2ac52471512a39007536153db012a33d0
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-14 11:23:07 +0200

    Add footer component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
 import MainContent from './components/Main/MainContent';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="app h-screen">
       <Header />
       <MainContent />
+      <Footer />
     </div>
   );
 }
