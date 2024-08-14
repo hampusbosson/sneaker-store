87757a9518721fdea2a0743d9564d0bdaf915900
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-14 17:41:58 +0200

    Create routes folder with a routes file that will contain all the routes to different pages, also add a errorPage that the user will be sent to when a route fails or does not exist.

---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import App from "../../App";
+import ErrorPage from "./ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "product/:item"
+  }
+];
+
+export default routes;
