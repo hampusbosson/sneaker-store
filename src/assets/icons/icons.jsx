99810aa88e5344708c57a42b1339e3509b65db98
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-14 23:11:52 +0200

    Add minus icon to be used in product page

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -58,6 +58,11 @@ const icons = {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-[1.3rem] mt-1">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
+    ),
+    productMinusIcon: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+        </svg>
     )
 }
 
