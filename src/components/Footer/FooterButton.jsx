b85f15f1c40a0fb9bc1bba085b63b57ac7b6b36e
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-14 15:29:14 +0200

    Create own component for footer-buttons and import that component into the main footer file for more flexibility

---
+++ src/components/Footer/FooterButton.jsx
@@ -0,0 +1,9 @@
+function FooterButton({title}) {
+    return (
+        <div>
+            <p className="cursor-pointer hover:underline">{title}</p>
+        </div>
+    )
+}
+
+export default FooterButton;
