5c8edadac0d72ced2f9792c52a2144aeb8a573cb
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-14 23:05:23 +0200

    make text for error page black

--- src/components/Routes/ErrorPage.jsx
+++ src/components/Routes/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <div className="text-black">
       <h1>Oh no, this route does not exist!</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
