a2a3dbc3c49dc28d98bb023aeceea579247be5d2
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-17 20:21:29 +0200

    Refactor productpage function

--- src/components/Routes/ProductPage/ProductPage.jsx
+++ src/components/Routes/ProductPage/ProductPage.jsx
@@ -3,6 +3,106 @@ import { useLocation /*useParams*/ } from "react-router-dom";
 import { useState } from "react";
 import icons from "../../../assets/icons/icons";
 
+function ProductShowcase({
+  state,
+  selectedSize,
+  aboutIsExpanded,
+  detailsIsExpanded,
+  handleSizeClick,
+  handleAboutSection,
+  handleDetailsSection,
+}) {
+  return (
+    <div className="lg:flex lg:flex-row lg:gap-20 lg:justify-between grid gap-8">
+      <img
+        src={state.imgSrc}
+        alt={state.alt}
+        className="w-[26rem] h-[17rem] shadow xl:w-[38rem] xl:h-[27rem] sm:w-[33rem] sm:h-[23rem]"
+      />
+      <div className="flex flex-col w-full">
+        <div className="flex justify-between">
+          <p className="text-gray-800 text-xl">{state.brand}</p>
+          {icons.productHeartIcon}
+        </div>
+        <div className="mt-2">
+          <p className="text-3xl font-bold">{state.name}</p>
+          <p className="font-semibold text-xl mt-0.5">{`$${state.price}`}</p>
+        </div>
+        <div className="flex gap-6 mt-6">
+          <p className="font-bold text-lg">Shoe Size (UK)</p>
+          <p className="font-semibold underline text-lg">Size Chart</p>
+        </div>
+        <div className="flex gap-2 mt-3">
+          {["4", "5", "6", "7", "8"].map((size) => (
+            <SizeButton
+              key={size}
+              size={size}
+              isSelected={selectedSize === size}
+              onClick={() => handleSizeClick(size)}
+            />
+          ))}
+        </div>
+        <button className="bg-black text-white mt-5 h-16 font-semibold text-lg">
+          Add to cart
+        </button>
+        <button
+          className="flex justify-between font-bold text-lg mt-6 border-b border-gray-400 pb-4"
+          onClick={handleAboutSection}
+        >
+          <p>ABOUT PRODUCT</p>
+          {aboutIsExpanded ? icons.productMinusIcon : icons.productPlusIcon}
+        </button>
+        {aboutIsExpanded && (
+          <div className="font-medium text-lg mt-2 mb-4 lg:max-w-[21rem] md:max-w-[30rem] sm:max-w-[30rem] max-w-[26rem]">
+            {state.description}
+          </div>
+        )}
+        <button
+          className="flex justify-between font-bold text-lg mt-4 border-b border-gray-400 pb-4"
+          onClick={handleDetailsSection}
+        >
+          <p>PRODUCT DETAILS</p>
+          {detailsIsExpanded ? icons.productMinusIcon : icons.productPlusIcon}
+        </button>
+        {detailsIsExpanded && (
+          <div>
+            <ul className="text-lg mt-2 lg:max-w-[21rem] md:max-w-[30rem] sm:max-w-[30rem] max-w-[26rem]">
+              <li className="flex gap-1">
+                <div className="font-bold">Manufacturer:</div>
+                <div>{state.brand}</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Weight:</div>
+                <div>1.20 KG</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Generic Name:</div>
+                <div>Shoe</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Unit of Measurement:</div>
+                <div>1 pair</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Release Date:</div>
+                <div>{state.releaseDate}</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Colorway:</div>
+                <div>{state.colorWay}</div>
+              </li>
+              <li className="flex gap-1">
+                <div className="font-bold">Article Code:</div>
+                <div>{state.articleCode}</div>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
 function SizeButton({ size, isSelected, onClick }) {
   return (
     <button
@@ -30,101 +130,34 @@ function ProductPage() {
 
   const handleAboutSection = () => {
     if (
-      aboutIsExpanded === true
+      (aboutIsExpanded === true
         ? setAboutIsExpanded(false)
-        : setAboutIsExpanded(true), setDetailsIsExpanded(false)
+        : setAboutIsExpanded(true),
+      setDetailsIsExpanded(false))
     );
   };
 
   const handleDetailsSection = () => {
     if (
-        detailsIsExpanded === true
-          ? setDetailsIsExpanded(false)
-          : setDetailsIsExpanded(true), setAboutIsExpanded(false)
-      );
+      (detailsIsExpanded === true
+        ? setDetailsIsExpanded(false)
+        : setDetailsIsExpanded(true),
+      setAboutIsExpanded(false))
+    );
   };
 
   return (
     <Layout>
       <div className="flex flex-col justify-center items-center text-black">
-        <div className="lg:flex lg:flex-row lg:gap-20 lg:justify-between grid gap-8">
-          <img
-            src={state.imgSrc}
-            alt={state.alt}
-            className="w-[26rem] h-[17rem] shadow xl:w-[38rem] xl:h-[27rem] sm:w-[33rem] sm:h-[23rem]"
-          />
-          <div className="flex flex-col w-full">
-            <div className="flex justify-between">
-              <p className="text-gray-800 text-xl">{state.brand}</p>
-              {icons.productHeartIcon}
-            </div>
-            <div className="mt-2">
-              <p className="text-3xl font-bold">{state.name}</p>
-              <p className="font-semibold text-xl mt-0.5">{`$${state.price}`}</p>
-            </div>
-            <div className="flex gap-6 mt-6">
-              <p className="font-bold text-lg">Shoe Size (UK)</p>
-              <p className="font-semibold underline text-lg">Size Chart</p>
-            </div>
-            <div className="flex gap-2 mt-3">
-              {["4", "5", "6", "7", "8"].map((size) => (
-                <SizeButton
-                  key={size}
-                  size={size}
-                  isSelected={selectedSize === size}
-                  onClick={() => handleSizeClick(size)}
-                />
-              ))}
-            </div>
-            <button className="bg-black text-white mt-5 h-16 font-semibold text-lg">
-              Add to cart
-            </button>
-            <button
-              className="flex justify-between font-bold text-lg mt-6 border-b border-gray-400 pb-4"
-              onClick={handleAboutSection}
-            >
-              <p>ABOUT PRODUCT</p>
-              {aboutIsExpanded ? icons.productMinusIcon : icons.productPlusIcon}
-            </button>
-            {aboutIsExpanded && <div className="font-medium text-lg mt-2 mb-4 lg:max-w-[21rem] md:max-w-[30rem] sm:max-w-[30rem] max-w-[26rem]">{state.description}</div>}
-            <button className="flex justify-between font-bold text-lg mt-4 border-b border-gray-400 pb-4"
-            onClick={handleDetailsSection}>
-              <p>PRODUCT DETAILS</p>
-              {detailsIsExpanded ? icons.productMinusIcon : icons.productPlusIcon}
-            </button>
-            {detailsIsExpanded && <div>
-              <ul className="text-lg mt-2">
-                <li className="flex gap-1">
-                  <div className="font-bold">Manufacturer:</div>
-                  <div>{state.brand}</div>
-                </li>
-                <li className="flex gap-1">
-                  <div className="font-bold">Weight:</div>
-                  <div>1.20 KG</div>
-                </li>
-                <li className="flex gap-1">
-                  <div className="font-bold">Generic Name:</div>
-                  <div>Shoe</div>
-                </li>
-                <li className="flex gap-1">
-                  <div className="font-bold">Unit of Measurement:</div>
-                  <div>1 pair</div>
-                </li>
-                <li className="flex gap-1">
-                  <div className="font-bold">Release Date:</div>
-                  <div>{state.releaseDate}</div>
-                </li>
-                <li className="flex gap-1">
-                  <div className="font-bold">Colorway:</div>
-                  <div>{state.colorWay}</div>
-                </li><li className="flex gap-1">
-                  <div className="font-bold">Article Code:</div>
-                  <div>{state.articleCode}</div>
-                </li>
-              </ul>
-              </div>}
-          </div>
-        </div>
+        <ProductShowcase
+          state={state}
+          selectedSize={selectedSize}
+          aboutIsExpanded={aboutIsExpanded}
+          detailsIsExpanded={detailsIsExpanded}
+          handleSizeClick={handleSizeClick}
+          handleAboutSection={handleAboutSection}
+          handleDetailsSection={handleDetailsSection}
+        />
       </div>
     </Layout>
   );
