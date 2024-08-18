1d8df8e5bb3b6e01a91948dbf086be7a5a383630
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-18 22:07:54 +0200

    Add logic for opening and closing the cartdialog when cartbutton is clicked.

--- src/components/Header/CartButton.jsx
+++ src/components/Header/CartButton.jsx
@@ -1,10 +1,25 @@
 import icons from '../../assets/icons/icons';
+import { useState } from 'react';
+import CartDialog from './CartDialog';
 
 function CartButton() {
+  const handleClick = () => {
+    setCartOpen(true)
+  }
+
+  const handleClose = () => {
+    setCartOpen(false);
+  };
+
+  const [cartOpen, setCartOpen] = useState(null);
+
   return (
-    <div>
-      {icons.cartIcon}
-    </div>
+    <>
+      <button onClick={handleClick}>
+        {icons.cartIcon}
+      </button>
+      <CartDialog isOpen={cartOpen} onClose={handleClose} />
+    </>
   );
 }
 
