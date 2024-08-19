ff8556a31901aef651c5f7bd36136290472417a5
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-19 22:42:35 +0200

    Add title prop to each contentbutton in headercontentList

--- src/components/Header/HeaderContentList/HeaderContentList.jsx
+++ src/components/Header/HeaderContentList/HeaderContentList.jsx
@@ -13,15 +13,15 @@ function HeaderContentList() {
         <div className="hidden lg:flex items-center justify-center w-full">
             <ul className="flex flex-row gap-x-12 gap-y-6 flex-wrap justify-center items-center w-10/12"> 
                 <ContentButton name='Shop All' onClick={() => handleClick('shop-all', 'Shop All')} />
-                <ContentButton name='New arrivals' onClick={() => handleClick('new-arrivals')} />
-                <ContentButton name='Nike Dunk' onClick={() => handleClick('nike-dunk')} />
-                <ContentButton name='Footwear' onClick={() => handleClick('footwear')} />
-                <ContentButton name='Apparel' onClick={() => handleClick('apparel')} />
-                <ContentButton name='Accesories' onClick={() => handleClick('accesories')} />
-                <ContentButton name='Blogs' onClick={() => handleClick('blogs')} />
-                <ContentButton name='Brands' onClick={() => handleClick('brands')} />
-                <ContentButton name='Releases' onClick={() => handleClick('releases')} />
-                <ContentButton name='Super Sale' onClick={() => handleClick('super-sale')} />
+                <ContentButton name='New arrivals' onClick={() => handleClick('new-arrivals', 'New Arrivals')} />
+                <ContentButton name='Nike Dunk' onClick={() => handleClick('nike-dunk', 'Nike Dunk')} />
+                <ContentButton name='Footwear' onClick={() => handleClick('footwear', 'Footwear')} />
+                <ContentButton name='Apparel' onClick={() => handleClick('apparel', 'Apparel')} />
+                <ContentButton name='Accesories' onClick={() => handleClick('accesories', 'Accesories')} />
+                <ContentButton name='Blogs' onClick={() => handleClick('blogs', 'Blogs')} />
+                <ContentButton name='Brands' onClick={() => handleClick('brands', 'Brands')} />
+                <ContentButton name='Releases' onClick={() => handleClick('releases', 'Releases')} />
+                <ContentButton name='Super Sale' onClick={() => handleClick('super-sale', 'Super sale')} />
             </ul>
         </div>
     );
