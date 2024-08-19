50aa9fa45f8e4743012572f2bd9508247c46fb6c
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-19 22:43:17 +0200

    Fix size and placement of cartdialog window.

--- src/components/Header/CartDialog.jsx
+++ src/components/Header/CartDialog.jsx
@@ -1,3 +1,5 @@
+import icons from "../../assets/icons/icons";
+
 function CartDialog({ isOpen, onClose }) {
   if (!isOpen) return null;
 
@@ -9,10 +11,10 @@ function CartDialog({ isOpen, onClose }) {
         className="fixed top-0 right-0 h-screen w-[25rem] z-10 bg-white shadow-lg m-0"
         style={{ inset: "0 0 0 auto" }}
       >
-        <div className="p-4">
+        <div className="p-4 flex justify-between">
           <h2>Your Cart</h2>
           {/* Cart items go here */}
-          <button onClick={onClose}>Close</button>
+          <button onClick={onClose}>{icons.closeIcon}</button>
         </div>
       </dialog>
     </>
