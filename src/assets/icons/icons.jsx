b92a36e7beb40163fb64d8b600daaefda48d9a10
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-19 22:46:32 +0200

    Fix strokewidth and size for closebutton-icon

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -65,7 +65,7 @@ const icons = {
         </svg>
     ),
     closeIcon: (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="w-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
     )
