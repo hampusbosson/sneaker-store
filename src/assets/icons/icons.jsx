4bc10009b5cc010b2cf007ee5ca73ff2280440a4
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-19 22:41:19 +0200

    Add close-icon to icons file

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -63,6 +63,11 @@ const icons = {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-[1.3rem] mt-1">
             <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
         </svg>
+    ),
+    closeIcon: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
     )
 }
 
