3e45d6ea4c98afd8a8c920cd8a9ab619872827ea
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-21 10:54:04 +0200

    Display title of each collection page

--- src/components/Routes/Collections/Collections.jsx
+++ src/components/Routes/Collections/Collections.jsx
@@ -1,13 +1,16 @@
 import Layout from "../../../Layout";
+//import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+//import CarouselButton from "../../Main/CarouselButton";
+//import { searchSneakers } from "../../../api";
 
 function Collections() {
     const { state } = useLocation();
-
+    
     return (
         <Layout>
             <div className="text-black">
-                {state.title}
+                <h1>{state.title}</h1>
             </div>
         </Layout>
     )
