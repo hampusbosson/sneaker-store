cb0cacad69f5662eea329ddfecf1d123125dfddb
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-21 10:55:23 +0200

    Rename Routes to AppRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from './components/Routes/Routes.jsx';
 import './index.css'
-
-const router = createBrowserRouter(routes);
+import AppRoutes from '../src/components/Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <AppRoutes />
+    </React.StrictMode>,
+  document.getElementById('root')
 )
