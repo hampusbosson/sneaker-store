0125e5e12cec8d068942e3ef269f5378e2d50c56
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-21 10:55:53 +0200

    Make sure AppRoutes is rendered globally

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import AppRoutes from '../src/components/Routes/Routes.jsx';
+import AppRoutes from './components/Routes/AppRoutes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
