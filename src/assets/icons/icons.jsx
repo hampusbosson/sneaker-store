deed793964365a38c9a426f138d0e97a1b429ed6
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-21 12:51:36 +0200

    Add new plus and minus icons for cartdialog counter

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -64,6 +64,16 @@ const icons = {
             <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
         </svg>
     ),
+    productPlusIconCart: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-[1.1rem] mx-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+    ),
+    productMinusIconCart: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-[1.1rem] mx-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+        </svg>
+    ),
     closeIcon: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="w-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
