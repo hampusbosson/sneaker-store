9018b8cac7bd467fc9858609a3862916a4a8d789
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-22 20:25:34 +0200

    Add route for login page to routes file

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import App from "../../App";
 import ErrorPage from "./ErrorPage";
 import ProductPage from "./ProductPage/ProductPage";
 import Collections from "./Collections/Collections";
+import LoginPage from './LoginPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
@@ -14,6 +15,7 @@ function AppRoutes() {
           <Route path="product/:item" element={<ProductPage />} />
           <Route path="collections/:collection" element={<Collections />} />
           <Route path="*" element={<ErrorPage />} />
+          <Route path="account/login" element={<LoginPage/>}/>
         </Routes>
       </BrowserRouter>
     </CartProvider>
