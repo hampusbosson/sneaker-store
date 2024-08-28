b5125adaa9f42899b353b0c26b5ec33e37848aba
Hampus Bosson <hampus.bosson@outlook.com>
2024-08-28 17:43:08 +0200

    Install react-query and wrap app component with queryClientProvider to improve performance

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppRoutes from './components/Routes/AppRoutes.jsx';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        </QueryClientProvider>
     </React.StrictMode>,
   document.getElementById('root')
 )
