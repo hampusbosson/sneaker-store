1e9c5a9b60c47353c1838a1e1e087baeeaf34e7a
Hampus Bosson <hampus.bosson@outlook.com>
2025-04-21 21:29:37 +0200

    fix: only show loading spinner for the div that is not yet loaded and not on the entire page

--- src/components/Routes/Collections/Collections.jsx
+++ src/components/Routes/Collections/Collections.jsx
@@ -14,11 +14,7 @@ function Collections() {
   } = useQuery(["sneakers", state.data, state.limit], () =>
     searchSneakers(state.data, state.limit),
   );
-
-  if (isLoading) {
-    return <div className="text-center mt-4 text-black">Loading...</div>;
-  }
-
+  
   if (error) {
     return (
       <div className="text-center mt-4 text-red-500">
@@ -32,21 +28,24 @@ function Collections() {
       <div className="text-black 2xl:mx-[12rem] xl:mx-[5rem] md:mx-[5rem] mx-[1rem] flex flex-col gap-4">
         <h1 className="font-bold text-4xl ml-6">{state.title}</h1>
         <div className="grid lg:grid-cols-3 grid-cols-2">
-          {sneakers.map((sneaker) => (
-            <div key={sneaker._id} className="xl:p-6 md:p-4 p-2">
-              <CarouselButton
-                imgSrc={sneaker.thumbnail}
-                alt={sneaker.shoeName}
-                brand={sneaker.brand}
-                name={sneaker.make}
-                price={sneaker.lowestResellPrice.stockX}
-                description={sneaker.description}
-                releaseDate={sneaker.releaseDate}
-                colorWay={sneaker.colorway}
-                articleCode={sneaker.styleID}
-              />
-            </div>
-          ))}
+          {isLoading ? (<div className="text-center mt-4 text-black">Loading...</div>) : 
+            (sneakers.map((sneaker) => (
+              <div key={sneaker._id} className="xl:p-6 md:p-4 p-2">
+                <CarouselButton
+                  imgSrc={sneaker.thumbnail}
+                  alt={sneaker.shoeName}
+                  brand={sneaker.brand}
+                  name={sneaker.make}
+                  price={sneaker.lowestResellPrice.stockX}
+                  description={sneaker.description}
+                  releaseDate={sneaker.releaseDate}
+                  colorWay={sneaker.colorway}
+                  articleCode={sneaker.styleID}
+                />
+              </div>
+            )))
+          }
+
         </div>
       </div>
     </Layout>
